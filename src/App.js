dfac66916fc279267da25f784ab215a72c95093a
jjanddak <opplane@gmail.com>
2021-04-15 19:01:27 +0900

    환경변수, map api 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'; // 패키지 불러오기
+require("dotenv").config();
+// import dotenv from "dotenv";
+// dotenv.config();
+
+function NaverMapAPI() {
+  return (
+    <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      style={{
+        width: '50%', // 네이버지도 가로 길이
+        height: '50vh' // 네이버지도 세로 길이
+      }}
+      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
+      defaultZoom={13} // 지도 초기 확대 배율
+    />
+  );
+}
 
 function App() {
+  const ClientId=process.env.REACT_APP_MAPS_CLIENT;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RenderAfterNavermapsLoaded
+        ncpClientId={ClientId} // 자신의 네이버 계정에서 발급받은 Client ID
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMapAPI />
+      </RenderAfterNavermapsLoaded>
     </div>
   );
 }
