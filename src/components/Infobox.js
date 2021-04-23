eaaac62295283734b9f7f458260ed8dad2a8e94a
jjanddak <opplane@gmail.com>
2021-04-23 18:04:20 +0900

    port to 8000, infobox add

--- src/components/Infobox.js
+++ src/components/Infobox.js
@@ -2,6 +2,7 @@ import "./css/Infobox.css";
 
 function Infobox () {
   return(
+    <>
     <div className="infobox" id="info1">
       <div className="infoTitleContainer">
         <span className="infoTitle">가산동</span>
@@ -9,44 +10,480 @@ function Infobox () {
       </div>
 
       <div className="infoUpperbox">
-        <div>
-          <span>총 운영개소 </span>
-          <span>316</span>
-          <span>개소</span>
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
         </div>
 
-        <div>
-          <div>CCTV</div>
-          <span>236</span>
-          <span>개</span>
-        </div>
-        <div>
-          <div>비상벨</div>
-          <span>80</span>
-          <span>개</span>
-        </div>
-        <div>
-          <div>IoT센서</div>
-          <span>8</span>
-          <span>개</span>
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
         </div>
       </div>
       
       <div className="infoLowerbox">
         <div>
           <span>선별관제 이벤트</span>
-          <span>168건</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
         </div>
         <div>
           <span>비상벨 이벤트</span>
-          <span>36건</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
         </div>
         <div>
           <span>관제업무 처리 이벤트</span>
-          <span>4건</span>
+          <span><span className="quantity" id="process">4</span>건</span>
         </div>
       </div>
     </div>
+
+
+    <div className="infobox" id="info2">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">독산1동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info3">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">시흥5동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info4">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">시흥3동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info5">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">시흥1동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info6">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">독산3동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info7">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">독산4동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info8">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">독산2동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info9">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">시흥4동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="infobox" id="info10">
+      <div className="infoTitleContainer">
+        <span className="infoTitle">시흥2동</span>
+        <span className="infoDetailbtn">+</span>
+      </div>
+
+      <div className="infoUpperbox">
+        <div className="infoTotal">
+          <span className="boxText">총 운영개소 </span>
+          <span><span className="quantity">316</span>개소</span>
+          
+        </div>
+
+        <div className="upperCategory">
+          <div>
+            <div>CCTV</div>
+            <span className="quantity" id="cctv">236</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>비상벨</div>
+            <span className="quantity" id="bell">80</span>
+            <span>개</span>
+          </div>
+          <div>
+            <div>IoT센서</div>
+            <span className="quantity" id="iot">8</span>
+            <span>개</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="infoLowerbox">
+        <div>
+          <span>선별관제 이벤트</span>
+          <span><span className="quantity" id="cctv">168</span>건</span>
+        </div>
+        <div>
+          <span>비상벨 이벤트</span>
+          <span><span className="quantity" id="bell">36</span>건</span>
+        </div>
+        <div>
+          <span>관제업무 처리 이벤트</span>
+          <span><span className="quantity" id="process">4</span>건</span>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
